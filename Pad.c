9ebf8c3a12a1c24766bb07eb7b3446bcfa6c5067
Mika Viinamäki <mika.viinamaki@fraktio.fi>
2019-02-03 22:02:43 +0200

    Initialize whole array properly

--- Pad.c
+++ Pad.c
@@ -3,15 +3,15 @@
 #include "Pad.h"
 
 PadConfiguration PAD_CONFIGURATION = {
-    .sensorThresholds = { 400 },
+    .sensorThresholds = { [0 ... SENSOR_COUNT - 1] = 400 },
     .basePressure = 0,
     .releaseMultiplier = 0.9,
     .oldValueWeight = 1
 };
 
 PadState PAD_STATE = { 
-    .sensorValues = { 0 },
-    .buttonsPressed = { false }
+    .sensorValues = { [0 ... SENSOR_COUNT - 1] = 0 },
+    .buttonsPressed = { [0 ... BUTTON_COUNT - 1] = false }
 };
 
 void Pad_UpdateState(const uint16_t newValues[SENSOR_COUNT]) {
